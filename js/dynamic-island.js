b6a8c1ef7c26872e8516c4462506d022eb264d4f
Okkst <99626693+Okkst00@users.noreply.github.com>
2022-10-15 18:23:23 +0700

    Add files via upload

---
+++ js/dynamic-island.js
@@ -0,0 +1,10 @@
+
+ const dynamicIslandPill = document.querySelector(".dynamic-island .pill");
+ const dynamicIsland = document.querySelector(".dynamic-island");
+ const container = document.querySelector(".dynamic-island .container");
+
+ dynamicIslandPill.addEventListener("click", () => {
+ dynamicIsland.classList.toggle("active");
+ container.classList.toggle("active");
+    }
+);
